ff7537a8533ddd89ec8367ecd091a08872092c36
dsweber2 <david.weber2@protonmail.com>
2021-10-06 14:42:31 -0700

    rg/ivy/counsel have issues with long lines no more

--- config.el
+++ config.el
@@ -1,19 +1,5 @@
-(setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-(setq doom-theme 'doom-one)
-
-(setq display-line-numbers-type `relative)
-
-(setq own-doom-home "/home/dsweber/.doom.d/")
-
-(add-hook 'prog-mode-hook 'subword-mode)
-
-(setq default-input-method "TeX")
-
-(setq create-lockfiles nil)
-
-(setq electric-pair-mode t)
+(after! +word-wrap
+  (setq +gobal-word-wrap-mode +1))
 
 (defun add-electric-pairs (new-pairs)
   (setq-local electric-pair-pairs (append electric-pair-pairs new-pairs)))
@@ -63,10 +49,6 @@
   (setq ispell-dictionary "en-custom")
   (setq ispell-personal-dictionary (concat own-doom-home "personal.txt"))
   )
-
-:config
-(setq custom-suffixes '(".pdf" ".png" ".svg"))
-(setq projectile-globally-ignored-file-suffixes (append projectile-globally-ignored-file-suffixes custom-suffixes))
 
 (use-package! org-ref
   :config
@@ -137,7 +119,8 @@
            (getenv "PATH")))
   (add-hook 'julia-mode-hook 'lsp)
   (add-hook 'ess-julia-mode-hook #'lsp)
-  (setq lsp-julia-default-environment "~/.julia/environments/v1.5")
+  (setq lsp-julia-default-environment "~/.julia/environments/v1.6")
+  (setq lsp-julia-package-dir "~/.julia/environments/v1.6")
 
   (setq lsp-julia-timeout 12000)
   ;; (add-to-list 'lsp-julia-flags (concat "-J /home/dsweber/julia-"
@@ -253,6 +236,29 @@
 (setq a-date 3425)
 (setq b-date 3295)
 
+(defun elfeed-score/toggle-debug-warn-level ()
+  (if (eq elfeed-score-log-level 'debug)
+      (setq elfeed-score-log-level 'warn)
+    (setq elfeed-score-log-level 'debug)))
+
+(map! :leader
+      (:prefix ("e" . "elfeed")
+       :desc "elfeed-score-map" "m" #'elfeed-score-map
+       :desc "open feed"        "f" #'elfeed
+       :desc "update elfeed"    "u" #'elfeed-update
+       :desc "score entries"    "s" #'elfeed-score/score
+       :desc "add score rules"  "r" #'elfeed-score-load-score-file
+       :desc "toggle debug"     "d" #'elfeed-score/toggle-debug-warn-level
+       )
+      )
+
+:config
+(setq custom-suffixes '(".pdf" ".png" ".svg"))
+(setq projectile-globally-ignored-file-suffixes (append projectile-globally-ignored-file-suffixes custom-suffixes))
+
+(after! counsel
+  (setq counsel-rg-base-command '("rg" "--max-columns" "900" "--with-filename" "--no-heading" "--line-number" "--color" "never" "%s")))
+
 (setq elfeed-log-level 'debug)
 (toggle-debug-on-error)
 (setq elfeed-protocol-log-trace t)
@@ -272,8 +278,3 @@
                               :autotags elfeed-protocol-tags))))
   (elfeed-protocol-enable)
   )
-
-(defun elfeed-score/toggle-debug-warn-level ()
-  (if (eq elfeed-score-log-level 'debug)
-      (setq elfeed-score-log-level 'warn)
-    (setq elfeed-score-log-level 'debug)))
