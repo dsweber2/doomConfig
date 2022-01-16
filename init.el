6138432be8ac83a398a27be8247e91ee7e37b4dd
dsweber2 <david.weber2@protonmail.com>
2022-01-15 23:27:00 -0800

    extending org mode to jupyter and blogging

--- init.el
+++ init.el
@@ -146,11 +146,11 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org                 ; organize your plain life in plain text
+       (org+jupyter+hugo)       ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp +conda); beautiful is better than ugly python
+       (python +lsp +conda + pyright); beautiful is better than ugly python
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
